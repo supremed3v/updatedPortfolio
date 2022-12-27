8e20e2fb8a3c2c260835ff14990520cb75ecdaec
supremed3v <saadsiddiqui-programmer@protonmail.com>
2022-12-27 12:15:35 +0500

    env setup

--- src/client.js
+++ src/client.js
@@ -1,14 +1,14 @@
-import sanityClient from '@sanity/client'
-import imageUrlBuilder from '@sanity/image-url'
+import sanityClient from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-    projectId: "htfg90et",
-    dataset: "portfolio_new",
-    apiVersion: "2020-02-01",
-    useCdn: true,
-    token: "skh7k0SxeAZCW5GgHjugkluQKFyYaM1vfa55eNuPdtTqTYseWCwTjzNYCATYeeuZMtmSUu1wNRSxfONYswmMT1wBepfIyaYEDdQxEDGB5GmZdlFPWzevI8OyqarZF5QbI9rGrZuJvS3I49tkb9Jfk29eG3SniZ3ieJ6HkKlE61vSxN6SywHF"
-})
+  projectId: "htfg90et",
+  dataset: "portfolio_new",
+  apiVersion: "2020-02-01",
+  useCdn: true,
+  token: process.env.SANITY_TOKEN,
+});
 
-const builder = imageUrlBuilder(client)
+const builder = imageUrlBuilder(client);
 
-export const urlFor = (source) => builder.image(source)
+export const urlFor = (source) => builder.image(source);
